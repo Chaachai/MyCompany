1715ed9c85765f02c12c3b938c8ea27d64158ab8
CHAACHAI Youssef <CHAACHAI Youssef@DESKTOP-ISAAC.mshome.net>
2019-05-10 17:01:34 +0100

    interface dyal login

--- src/mycompany_v1/pkg1/LoginFXMLController.java
+++ src/mycompany_v1/pkg1/LoginFXMLController.java
@@ -7,9 +7,17 @@ package mycompany_v1.pkg1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 
 /**
  * FXML Controller class
@@ -17,9 +25,38 @@ import javafx.scene.control.Button;
  * @author CHAACHAI Youssef
  */
 public class LoginFXMLController implements Initializable {
-    
+
     @FXML
     private Button loginButton;
+
+    @FXML
+    private Label close;
+    @FXML
+    private Button minimize;
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private TextField login;
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    public void connect(){
+        System.out.println("Login = "+login.getText()+" Password = "+password.getText());
+    }
+
+    @FXML
+    public void closeApp() {
+        Stage stage = (Stage) close.getScene().getWindow();
+
+        stage.close();
+    }
+    
+    @FXML
+    public void minimizeApp(ActionEvent event) {
+        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
 
     /**
      * Initializes the controller class.
@@ -27,6 +64,7 @@ public class LoginFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        
+    }
+
 }
--- src/mycompany_v1/pkg1/MyCompany_v11.java
+++ src/mycompany_v1/pkg1/MyCompany_v11.java
@@ -6,24 +6,51 @@
 package mycompany_v1.pkg1;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author CHAACHAI Youssef
  */
 public class MyCompany_v11 extends Application {
-    
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("LoginFXML.fxml"));
+
         Scene scene = new Scene(root);
-        
+
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        Platform.runLater(() -> root.requestFocus());
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
@@ -31,7 +58,8 @@ public class MyCompany_v11 extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         launch(args);
     }
-    
+
 }
