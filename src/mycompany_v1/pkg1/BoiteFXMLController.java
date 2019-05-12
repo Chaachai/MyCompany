475c0f9d0b75c201d53640088f5cb8f3aa58fc3d
hamza <hamza@DESKTOP-MFUTTQO>
2019-05-12 03:23:19 +0000

    added forward function

---
+++ src/mycompany_v1/pkg1/BoiteFXMLController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompany_v1.pkg1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author hamza
+ */
+public class BoiteFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
